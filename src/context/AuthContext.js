c387c31084c4dd787035b0e9f9f16179ade02c93
Mandala Kuele <mandala@Mandalas-MacBook-Air.local>
2022-08-05 15:01:50 +0300

    commit

---
+++ src/context/AuthContext.js
@@ -0,0 +1,50 @@
+import { createContext, useContext, useState, useEffect } from 'react';
+import { auth, db } from '../firebase';
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+} from 'firebase/auth';
+import { setDoc, doc } from 'firebase/firestore';
+
+const AuthContext = createContext();
+
+export function AuthContextProvider({ children }) {
+  const [user, setUser] = useState({});
+
+  const signUp = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+    setDoc(doc(db, 'users', email), {
+      savedShows: [],
+    });
+    console.log(doc);
+  };
+
+  const logIn = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const logOut = () => {
+    return signOut(auth);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => {
+      unsubscribe();
+    };
+  });
+
+  return (
+    <AuthContext.Provider value={{ signUp, logIn, logOut, user }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export function UserAuth() {
+  return useContext(AuthContext);
+}
